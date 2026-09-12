a23413f8c9e6df32bea2d0394a5f2e6973380380
saviom2025 <msavio430@gmail.com>
2026-09-11 21:49:28 -0300

    feat: add is menu navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
+import { FaArrowRightLong } from "react-icons/fa6";
 export default function Home() {
   return (
     <>
-      <h1> Experience the shadcn landing page </h1>
-      <p>
-        We're more than just a tool, we're a community of passionate creator.
-        Get, access to exclusive resources, tutorials, and support.
-      </p>
+      <main>
+        <div className="bar">
+          <a href="#">New</a>
+          <a href="#">Design is out now?</a>
+        </div>
+        <h1> Experience the shadcn landing page </h1>
+        <p>
+          We're more than just a tool, we're a community of passionate creator.
+          Get, access to exclusive resources, tutorials, and support.
+        </p>
+        <button>
+          get started <FaArrowRightLong />
+        </button>
+        <button>github respository</button>
+      </main>
     </>
   );
 }
